18f06211de3c1e5293c3711034ed6374d6340e57
ichione158 <duynguyen15081998z@gmail.com>
2019-06-27 16:26:57 +0700

    Chinh sua layout, tao user

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
+  resources :users
   root 'application#hello'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_054543) do
+ActiveRecord::Schema.define(version: 2019_06_27_023521) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "image"
+    t.integer "phone"
+    t.datetime "date"
   end
 
 end
